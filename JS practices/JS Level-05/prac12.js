3f45142857c27991b89c491f196cb4b64c63bb99
FiaruProject <111216829+FiaruProject@users.noreply.github.com>
2025-08-31 15:20:44 +0600

    prac 12 added

---
+++ JS practices/JS Level-05/prac12.js
@@ -0,0 +1,31 @@
+// Introduction to Class and objects
+
+// Person => object
+
+// constructor() don't need to call . it can alyeady called by default when you create the object
+
+// without constructor() if you add new method like sepcial function => need to call 
+
+class Person {
+    constructor(name, age, mobileNumber) {
+        this.name = name;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+    }
+
+    goal(aim){
+        this.aim = aim;
+    }
+}
+
+let person1 = new Person('Arafath', 24, 18668389910); // constructor() already called by default
+
+person1.goal('Doctor'); // method like special funtion called
+
+let person2 = new Person('Nazafi', 25, 18778379912); // constructor() already called by default
+
+person2.goal('Engineer'); // method like special funtion called
+
+console.log(person1);
+
+console.log(person2);
